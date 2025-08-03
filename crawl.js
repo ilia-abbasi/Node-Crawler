d97e65cc97295bd7d0b14b5c37ba520903d6d242
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-03 22:32:47 +0330

    crawlPage and main.js code
    - Added crawlPage function to crawl.js
    
    - Added code to main function on main.js

--- crawl.js
+++ crawl.js
@@ -32,6 +32,45 @@ function getURLsFromHTML(htmlBody, baseURL) {
   return urls;
 }
 
+async function crawlPage(url) {
+  console.log("Checking URL's validity ...");
+
+  if (!isValidURL(url)) {
+    console.error(
+      `
+Invalid URL. Check if the link you provided is formatted correctly as a valid URL.
+You may have forgotten to include the protocol of the URL.
+      `
+    );
+    process.exit(1);
+  }
+
+  console.log("Sending GET request to URL ...");
+
+  try {
+    const resp = await fetch(url);
+    if (resp.status > 399) {
+      console.log(
+        `Error: Status code ${resp.status} was recieved while fetching ${url}`
+      );
+      return;
+    }
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `Error: Response was ${contentType} instead of HTML while fetching ${url}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(`Something went wrong while fetching ${url} :`);
+    console.log(err);
+  }
+}
+
 function isValidURL(string) {
   try {
     new URL(string);
@@ -44,4 +83,6 @@ function isValidURL(string) {
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
+  isValidURL,
+  crawlPage,
 };
--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const { crawlPage } = require("./crawl.js");
+const argv = process.argv;
+
 function main() {
-  //
+  argv.splice(0, 2);
+
+  if (argv.length !== 1) {
+    console.log(`Exactly 1 argument is expected. You provided ${argv.length}.`);
+    process.exit(1);
+  }
+
+  const baseURL = argv[0];
+
+  console.log(`Preparing to crawl ${baseURL} ...`);
+
+  crawlPage(baseURL);
 }
 
 try {
