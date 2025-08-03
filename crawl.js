c30ee6d720dbc2c1fc67d399bb3afcdf753bd14e
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-03 17:23:34 +0330

    getURLsFromHTML definition
    - Added code block to getURLsFromHTML function in crawl.js
    
    - Added tests for the said function in crawl.test.js
    
    - Added isValidURL function to crawl.js

--- crawl.js
+++ crawl.js
@@ -1,8 +1,8 @@
+const { JSDOM } = require("jsdom");
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   let result = `${urlObj.hostname}${urlObj.pathname}`;
-
-  //result = result.toLowerCase();
 
   if (result.length >= 1 && result.slice(-1) === "/") {
     result = result.slice(0, -1);
@@ -13,7 +13,32 @@ function normalizeURL(urlString) {
 
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  for (const linkElement of linkElements) {
+    let url = linkElement.href;
+
+    if (url.charAt(0) == "/") {
+      url = `${baseURL}${url}`;
+    }
+    if (!isValidURL(url)) {
+      continue;
+    }
+
+    urls.push(normalizeURL(url));
+  }
+
   return urls;
+}
+
+function isValidURL(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 module.exports = {
