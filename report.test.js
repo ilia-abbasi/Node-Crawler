299555db6d5b40d3b6450b58c3aedd3971ff14f0
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-04 09:37:22 +0330

    report.js and report.test.js
    - Added report.js
    
    - Added report.test.js
    
    - Added sortPages function to report.js
    
    - Added sortPages easy and complex tests to report.test.js

---
+++ report.test.js
@@ -0,0 +1,40 @@
+const { sortPages } = require("./report.js");
+const { test, expect } = require("@jest/globals");
+
+test("sortPages easy", () => {
+  const input = {
+    "https://example.com": 1,
+    "https://example.com/path": 3,
+  };
+  const expected = [
+    ["https://example.com/path", 3],
+    ["https://example.com", 1],
+  ];
+  const actual = sortPages(input);
+  expect(actual).toEqual(expected);
+});
+
+test("sortPages complex", () => {
+  const input = {
+    "https://example.com": 1,
+    "https://example.com/path": 7,
+    "https://example.com/path/to": 2,
+    "https://example.com/about": 5,
+    "https://example.com/tags": 3,
+    "https://example.com/path/to/there": 6,
+    "https://example.com/contact": 9,
+    "https://example.com/profile": 4,
+  };
+  const expected = [
+    ["https://example.com/contact", 9],
+    ["https://example.com/path", 7],
+    ["https://example.com/path/to/there", 6],
+    ["https://example.com/about", 5],
+    ["https://example.com/profile", 4],
+    ["https://example.com/tags", 3],
+    ["https://example.com/path/to", 2],
+    ["https://example.com", 1],
+  ];
+  const actual = sortPages(input);
+  expect(actual).toEqual(expected);
+});
