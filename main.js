f0332fdb10b082486ac4434541ee3bd19b001f50
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-04 09:54:31 +0330

    printReport in report.js and using in main.js
    - Added printReport function to report.js
    
    - Used printReport in main.js
    
    This is the last commit based on the HTTP networking course on FreeCodeCamp Youtube

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage } = require("./crawl.js");
+const { printReport } = require("./report.js");
 const argv = process.argv;
 
 async function main() {
@@ -14,9 +15,7 @@ async function main() {
   console.log(`Preparing to crawl ${baseURL} ...`);
 
   const pages = await crawlPage(baseURL, baseURL, {});
-  for (const page of Object.entries(pages)) {
-    console.log(page);
-  }
+  printReport(pages);
 }
 
 try {
--- report.js
+++ report.js
@@ -1,3 +1,20 @@
+function printReport(pages) {
+  console.log("--------------");
+  console.log("|   REPORT   |");
+  console.log("--------------");
+  console.log("|");
+  const sortedPages = sortPages(pages);
+  for (const sortedPage of sortedPages) {
+    const url = sortedPage[0];
+    const hits = sortedPage[1];
+    console.log(`|- Found ${hits} links to ${url}`);
+  }
+  console.log("|");
+  console.log("--------------");
+  console.log("| END REPORT |");
+  console.log("--------------");
+}
+
 function sortPages(pages) {
   const pagesArr = Object.entries(pages);
   pagesArr.sort((a, b) => {
@@ -10,4 +27,5 @@ function sortPages(pages) {
 
 module.exports = {
   sortPages,
+  printReport,
 };
